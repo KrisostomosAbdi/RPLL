9c16576a25e0b17bbbe2a4223268e4573a6e0244
CandhyFadhila <omaplocandhy@gmail.com>
2022-01-04 10:11:43 +0700

    WEB RPLL

--- WEB_RPLL/index_2.php
+++ WEB_RPLL/index_2.php
@@ -500,7 +500,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-6 col-md-12">
-					<p>Copyrights &copy; 2019 - <a href="https://imransdesign.com/">Imran Hossain</a>, All Rights Reserved.</p>
+					<p>Copyrights &copy; 2015 - 2020 <a href="https://dinus.ac.id/"> PSI UDINUS</a>, All Rights Reserved.</p>
 				</div>
 				<div class="col-lg-6 text-right col-md-12">
 					<div class="social-icons">
