78a93aa0275309d0c2f2561f44758b95359b1649
Krisostomos Abdixta Winarto <111201911695@mhs.dinus.ac.id>
2022-01-04 12:26:37 +0700

    WEB RPLL

--- WEB_RPLL/index_2.php
+++ WEB_RPLL/index_2.php
@@ -539,6 +539,18 @@
 	<!-- main js -->
 	<script src="assets/js/main.js"></script>
 
+	<!-- Start of ChatBot (www.chatbot.com) code -->
+	<script type="text/javascript">
+		window.__be = window.__be || {};
+		window.__be.id = "61d3d475e81e0b000794ba07";
+		(function() {
+			var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+			be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+		})();
+	</script>
+	<!-- End of ChatBot code -->
+
 </body>
 
 </html>
